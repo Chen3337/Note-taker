fcebdf2da4fa9c0a67255e783256f6948c62783f
zhen yong chen <dannychen111@yahoo.com>
2019-12-01 22:56:37 -0600

    added id for all notes and save and read from the db.json file

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const mysql = require("mysql");
 var path = require("path");
+const fs = require("fs");
 const PORT = process.env.port || 3500;
+let notes;
 
-const notes = [];
-
+const dataSaved =  fs.readFileSync("./db/db.json","UTF-8");
+if(dataSaved){
+    const oldNotes = JSON.parse(dataSaved);
+    notes = oldNotes;
+}
+else{
+    notes = [];
+}
+creatID();
+console.log(notes);
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,11 +36,21 @@ app.get("/api/notes", function(req, res) {
 app.post("/api/notes", function(req, res) {
     let newNote = req.body;
     notes.push(newNote);
+    creatID();
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), function(err){if(err) throw err});
+});
+// delete notes from api
+app.delete("/api/notes/:id", function(req, res) {
+    console.log(req.param.id);
 });
 
 
 
-
+function creatID (){
+    for(i = 0; i < notes.length; i++){
+        notes[i].id = i;
+    }
+}
 
 
 
